0468fb1d9a5e1f9b90b87c59a1e49e8883abb33a
Guilherme <guilhermego1997@gmail.com>
2020-05-18 19:52:33 -0300

    Inclusão do pacote FluentAssertions

--- Jobs.Testes/TesteSequenciaJob.cs
+++ Jobs.Testes/TesteSequenciaJob.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Xunit;
 using SchedulingJob.Service;
 using System.Linq;
+using FluentAssertions;
 
 namespace Jobs.Testes
 {
@@ -13,7 +14,12 @@ namespace Jobs.Testes
         {
             var idJob = JobService.OrganizarJobs().Select(x => x.Id);
 
-            Assert.Equal(idJob, new List<int> { 1, 3, 2 });
+            idJob.Should().Equal(TesteSquenciaJob.ObterJobsEsperados(), $"A ordem dos Jobs não correspondem com o esperado");
+        }
+
+        public static List<int> ObterJobsEsperados()
+        {
+            return new List<int> { 1, 2, 2 };
         }
     }
 }
